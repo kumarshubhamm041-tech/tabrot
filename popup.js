7c64067c8b547eda41cf3ba678bbfe06bf4716cf
Shubham Kumar <kumarshubhamm041-tech@users.noreply.github.com>
2026-06-09 19:28:28 +0530

    scaffolded out popup.js setting loader. storage query triggers on DOM ready lmaoo.

---
+++ popup.js
@@ -0,0 +1,42 @@
+
+
+
+document.addEventListener("DOMContentloaded", () => {
+    const button = document.getElementById("global-toggle")
+    const threshold_select = document.getElementById("threshold-select");
+    const test_rot_btn= document.getElementById("test-rot-btn");
+    const reset_all_btn = document.getElementById("reset-all-btn");
+    const status_display = document.getElementById("status-display");
+
+
+    function showStatus(text, isError = false) {
+        status_display.textContent = text;
+    status_display.style.color = isError ? "#ff5e62" : "00f2fe";
+    setTimeout(() =>{
+    status_display.innerText = "settings synchronised";
+    status_display.style.color = "#oof2fe";
+    }, 1500);
+    }
+
+
+ chrome.storage.local.get(["decay_threshold_minutes", "global_enabled"], (settings) => {
+      if (chrome.runtime.lastError){
+        showStatus("failed loading storage", true);
+        return;
+      }
+
+
+      if (settings.global_enabled !== undefined) {
+        global_toggle.checked = settings.global_enabled;
+      }
+
+      if (settings.decay_threshold_minutes) {
+        threshold_select.value = settings.decay_threshold_minutes.toString();
+      }
+
+      console.log("storage sync successful finally");
+ });
+});
+
+
+      
